4f4d1882f1d1a1deda06bdb565c38d47b35b2faa
HugoDF <hdifrancesco@gmail.com>
2015-11-10 10:55:50 +0000

    initial commit

---
+++ app/scripts/application.js
@@ -0,0 +1,12 @@
+$(function(){
+  $('.nav-link.signup').on('click', function(e){
+    $('.signup-modal').toggleClass('closed');
+    e.preventDefault();
+  });
+  $('.signup-modal').on('click', function(){
+    $('.signup-modal').addClass('closed');
+  });
+  $('.signup-modal .modal-inner').on('click', function(e){
+    e.stopPropagation();
+  });
+});
